bc1311fe669857511874c6d9f8a6093009e4394b
Grace Frank <frankgracey@gmail.com>
2019-03-14 22:54:20 +0100

    added test cases for user class

--- user-class/user.test.js
+++ user-class/user.test.js
@@ -6,4 +6,46 @@ const dareLawal = new User(
   "lawal123hospital"
 );
 
-test("that ");
+test("that User constructor returns an object", () => {
+  expect(typeof dareLawal).toBe("object");
+});
+
+describe("the properties corresponds with the prarameters passed", () => {
+  test("name property corresponds to name passed", () => {
+    expect(dareLawal.name).toBe("Dare Lawal");
+  });
+  test("name property corresponds to name passed", () => {
+    expect(dareLawal.email).toBe("darelawal@yahoo.com");
+  });
+  test("name property corresponds to name passed", () => {
+    expect(dareLawal.password).toBe("lawal123hospital");
+  });
+});
+
+describe("testing the updateRecord Method of userClass", () => {
+  test("that if password is wron object properties are not updated", () => {
+    dareLawal.updateRecord("lawal777hospital", {
+      name: "Lawal Solomon",
+      email: "darelawal@yahoo.com"
+    });
+    expect(dareLawal.name).toBe("Dare Lawal");
+    expect(dareLawal.email).toBe("darelawal@yahoo.com");
+  });
+
+  test("that object properties are updated with correct details ", () => {
+    dareLawal.updateRecord("lawal123hospital", {
+      name: "Lawal Solomon",
+      email: "darelawal@yahoo.com"
+    });
+    expect(dareLawal.name).toBe("Lawal Solomon");
+    expect(dareLawal.email).toBe("darelawal@yahoo.com");
+  });
+});
+
+test("test that users can view their personal details ", () => {
+  expect(dareLawal.viewPersonalDetails()).toEqual({
+    name: "Lawal Solomon",
+    email: "darelawal@yahoo.com",
+    password: "lawal123hospital"
+  });
+});
