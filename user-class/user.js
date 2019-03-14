1607f7fba68908dee081bcd26a22ee90dc97e85d
Grace Frank <frankgracey@gmail.com>
2019-03-14 16:50:12 +0100

    defined properties and methods for user class

--- user-class/user.js
+++ user-class/user.js
@@ -1,5 +1,17 @@
 class User {
-
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  updateRecord(update) {
+    for (const key in update) {
+      this[key] = update[key];
+    }
+  }
+  viewPersonalRecord() {
+    return Object.toString(this);
+  }
 }
 
 module.exports = User;
