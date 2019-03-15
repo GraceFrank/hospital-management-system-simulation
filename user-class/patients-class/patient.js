955b8d54279b37331e3f22a822143c227bb93014
Grace Frank <frankgracey@gmail.com>
2019-03-15 13:12:25 +0100

    added methods for patient class

--- user-class/patients-class/patient.js
+++ user-class/patients-class/patient.js
@@ -1,3 +1,31 @@
-class Patient {}
+const User = require("../user");
+const dataManager = require("../../data-manager/data-manager");
+
+class Patient extends User {
+  constructor(userCategory, name, email, password) {
+    super(userCategory, name, email, password);
+  }
+
+  bookDoctorsAppointment(dateTime, doctor) {
+    dataManager.bookDoctorsAppointment(
+      this.userCategory,
+      dateTime,
+      doctor,
+      this.email
+    );
+  }
+  cancelDoctorsAppointment(dateTime, doctor) {
+    dataManager.cancelDoctorsAppointment(
+      this.userCategory,
+      dateTime,
+      doctor,
+      this.email
+    );
+  }
+
+  viewAppointments() {
+    dataManager.viewAppointments(this.userCategory, this.email);
+  }
+}
 
 module.exports = Patient;
